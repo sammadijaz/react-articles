c55f271438d3cb03c470a93a2d8ecd65d72be230
Sammad Ijaz <sammadijaz11223@gmail.com>
2026-05-29 08:24:23 +0500

    feat: update ArticlePage to display article title and content based on route parameter

--- frontend/src/pages/ArticlePage.jsx
+++ frontend/src/pages/ArticlePage.jsx
@@ -1,11 +1,19 @@
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
+import articles from "../content/article-content";
 
 const ArticlePage = () => {
   const { name } = useParams();
 
-  return (
-    <h1>This is {name} Article</h1>
-  )
-}
+  const article = articles.find((a) => a.name === name);
 
-export default ArticlePage
+  return (
+    <>
+      <h1>{article.title}</h1>
+      {article.content.map((p) => (
+        <p key={p}>{p}</p>
+      ))}
+    </>
+  );
+};
+
+export default ArticlePage;
