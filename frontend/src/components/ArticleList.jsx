b3ea4ea684b6c30e2cda737a68274c47b367746f
Sammad Ijaz <sammadijaz11223@gmail.com>
2026-05-30 02:14:13 +0500

    feat: refactor ArticlesListPage to use ArticleList component for rendering articles

--- frontend/src/components/ArticleList.jsx
+++ frontend/src/components/ArticleList.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
-const ArticleList = () => {
+const ArticleList = ({ articles }) => {
   return (
-    <div>ArticleList</div>
+     <>
+      <h1>Articles</h1>
+      {articles.map((a) => (
+        <Link key={a.name} to={"/articles/" + a.name}>
+          <h3>{a.title}</h3>
+          <p>{a.content[0].substring(0, 150)}</p>
+        </Link>
+      ))}
+    </>
   )
 }
 
--- frontend/src/pages/ArticlesListPage.jsx
+++ frontend/src/pages/ArticlesListPage.jsx
@@ -1,17 +1,10 @@
 import { Link } from "react-router-dom";
 import articles from "../content/article-content";
+import ArticleList from "../components/ArticleList";
 
 const ArticlesListPage = () => {
   return (
-    <>
-      <h1>Articles</h1>
-      {articles.map((a) => (
-        <Link key={a.name} to={"/articles/" + a.name}>
-          <h3>{a.title}</h3>
-          <p>{a.content[0].substring(0, 150)}</p>
-        </Link>
-      ))}
-    </>
+    <ArticleList articles={articles}/>
   );
 };
 
