28fbf14f17bcdfc68a3c0ad18632ca5b985944e9
Sammad Ijaz <sammadijaz11223@gmail.com>
2026-05-29 07:05:16 +0500

    feat: add routing for AboutPage, ArticlesList, and ArticlePage components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,25 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ArticlesList from "./pages/ArticlesList";
+import ArticlePage from "./pages/ArticlePage";
 
 const routes = [
   {
     path: "/",
     element: <HomePage />,
+  },
+  {
+    path: "/about",
+    element: <AboutPage />,
+  },
+  {
+    path: "/articles",
+    element: <ArticlesList />,
+  },
+  {
+    path: "/articles/article",
+    element: <ArticlePage />,
   },
 ];
 
