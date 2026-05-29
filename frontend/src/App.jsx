4c3c7f70394ba4484a352382a44a9c6181e7d337
Sammad Ijaz <sammadijaz11223@gmail.com>
2026-05-29 07:28:35 +0500

    feat: implement Layout component with Navbar and routing structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,24 +3,31 @@ import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import ArticlesList from "./pages/ArticlesList";
 import ArticlePage from "./pages/ArticlePage";
+import Layout from "./Layout";
 
 const routes = [
   {
     path: "/",
-    element: <HomePage />,
-  },
-  {
-    path: "/about",
-    element: <AboutPage />,
-  },
-  {
-    path: "/articles",
-    element: <ArticlesList />,
-  },
-  {
-    path: "/articles/article",
-    element: <ArticlePage />,
-  },
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/about",
+        element: <AboutPage />,
+      },
+      {
+        path: "/articles",
+        element: <ArticlesList />,
+      },
+      {
+        path: "/articles/article",
+        element: <ArticlePage />,
+      },
+    ],
+  }
 ];
 
 const router = createBrowserRouter(routes);
