0c259f3573c2926155969433330432294925a94b
Sammad Ijaz <sammadijaz11223@gmail.com>
2026-05-29 07:11:20 +0500

    feat: make Navbar component

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Articles</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
