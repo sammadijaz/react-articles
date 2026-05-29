e005ce3ef07597bd836d2aae7a4b3c81c95fad5d
Sammad Ijaz <sammadijaz11223@gmail.com>
2026-05-30 02:05:17 +0500

    feat: implement ArticlesList component to display dynamic articles with titles and excerpts

--- frontend/src/pages/ArticlesList.jsx
+++ frontend/src/pages/ArticlesList.jsx
@@ -1,7 +1,18 @@
+import { Link } from "react-router-dom";
+import articles from "../content/article-content";
+
 const ArticlesList = () => {
   return (
-    <div>ArticlesList</div>
-  )
-}
+    <>
+      <h1>Articles</h1>
+      {articles.map((a) => (
+        <Link key={a.name} to={"/articles/" + a.name}>
+          <h3>{a.title}</h3>
+          <p>{a.content[0].substring(0, 150)}</p>
+        </Link>
+      ))}
+    </>
+  );
+};
 
-export default ArticlesList
+export default ArticlesList;
